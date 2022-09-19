e435e6699f82daf7e4fc6b1c39785f7e61a93a73
uguroban <36131157+uguroban@users.noreply.github.com>
2022-09-19 17:13:04 +0300

    UnitTest project added

---
+++ JobApplicationLibrary.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace JobApplicationLibrary.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
