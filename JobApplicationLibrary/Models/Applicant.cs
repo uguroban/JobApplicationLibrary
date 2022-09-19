ef1827cb520a1f26d0afdf203b10c66f1ddc2964
uguroban <36131157+uguroban@users.noreply.github.com>
2022-09-19 20:47:05 +0300

    Applicant new properties added

--- JobApplicationLibrary/Models/Applicant.cs
+++ JobApplicationLibrary/Models/Applicant.cs
@@ -2,6 +2,10 @@
 {
     public class Applicant
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BofYear { get; set; }
+        public string IdentityNumber { get; set; }
         public int Age { get; set; }
     }
 }
