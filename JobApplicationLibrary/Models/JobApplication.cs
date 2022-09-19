a54d04ff8376c5aaa7300149f2cb11a24a810773
uguroban <36131157+uguroban@users.noreply.github.com>
2022-09-19 15:37:57 +0300

    JobApplication properties defined

--- JobApplicationLibrary/Models/JobApplication.cs
+++ JobApplicationLibrary/Models/JobApplication.cs
@@ -2,5 +2,10 @@
 {
     public class JobApplication
     {
+        public Applicant Applicant { get; set; }
+
+        public int YearsofExperience { get; set; }
+
+        public List<string> TechStackList { get; set; }
     }
 }
