69b19c5a222d084572c84e789362a7c92053f972
uguroban <36131157+uguroban@users.noreply.github.com>
2022-09-19 20:26:46 +0300

    unit tests added

--- JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -28,7 +28,48 @@ namespace JobApplicationLibrary.UnitTest
 
         }
 
-        
-       
+        [Test]
+        public void Application_WithNoTechStack_TransfferedtoAutoRejected()
+        {
+            //Arrange
+            var evaluator=new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant() { Age=20},
+                TechStackList = new System.Collections.Generic.List<string>() { "" }
+
+            };
+
+            //Action
+            var result= evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(ApplicationResult.AutoRejected,result);
+
+        }
+
+        [Test]
+        public void Application_WithHighTechStack_TransfferedtoAutoAccepted()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant() { Age=25},
+                TechStackList = new System.Collections.Generic.List<string>() { "C#", "RabbitMQ", "MicroServices", "Visual Studio" },
+                YearsofExperience=15
+                
+
+            };
+
+            //Action
+            var result = evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(ApplicationResult.AutoAccepted, result);
+
+        }
+
+
     }
 }
--- JobApplicationLibrary/ApplicationEvaluator.cs
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -5,6 +5,8 @@ namespace JobApplicationLibrary
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearsofExperience = 5;
+        private List<string> techStackList = new() {"C#","RabbitMQ","MicroServices","Visual Studio" };
         //Unit of Work
         public ApplicationResult Evaluate(JobApplication form)
         {
@@ -12,7 +14,24 @@ namespace JobApplicationLibrary
             {
                 return ApplicationResult.AutoRejected;
             }
+
+            var sr = GetTechStackRate(form.TechStackList);
+            if (sr<25)
+            {
+                return ApplicationResult.AutoRejected;
+            }
+            if (sr>75 && form.YearsofExperience >= autoAcceptedYearsofExperience)
+            {
+                return ApplicationResult.AutoAccepted;
+            }
             return ApplicationResult.AutoAccepted;
+        }
+
+        private int GetTechStackRate(List<string> userTechStacks)
+        {
+            var matchCount = userTechStacks.Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase)).Count();
+            return (int)(double)(matchCount/techStackList.Count())*100;
+            
         }
     }
 
